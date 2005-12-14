83f59f6a29d696c70300e0017969855aa2240bee
pwelter34 <pwelter34@299a232d-b705-0410-b782-f21f2f1e606a>
2005-12-14 00:36:45 +0000

    Started work on ServiceController
    git-svn-id: http://msbuildtasks.tigris.org/svn/msbuildtasks/trunk@53 299a232d-b705-0410-b782-f21f2f1e606a

--- Source/MSBuild.Community.Tasks/ServiceController.cs
+++ Source/MSBuild.Community.Tasks/ServiceController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.Build.Utilities;
+using Service = System.ServiceProcess;
 
 namespace MSBuild.Community.Tasks
 {
@@ -10,6 +11,44 @@ namespace MSBuild.Community.Tasks
     /// </summary>
     public class ServiceController : Task
     {
+
+        public ServiceController()
+        {
+
+        }
+
+        private string _serviceName;
+
+        public string ServiceName
+        {
+            get { return _serviceName; }
+            set { _serviceName = value; }
+        }
+
+        private string _machineName;
+
+        public string MachineName
+        {
+            get { return _machineName; }
+            set { _machineName = value; }
+        }
+
+        private string _action;
+
+        public string Action
+        {
+            get { return _action; }
+            set { _action = value; }
+        }
+
+        private double _timeout;
+
+        public double Timeout
+        {
+            get { return _timeout; }
+            set { _timeout = value; }
+        }            
+        
         /// <summary>
         /// Executes the task.
         /// </summary>
@@ -17,7 +56,14 @@ namespace MSBuild.Community.Tasks
         /// otherwise <see langword="false"/>.</returns>
         public override bool Execute()
         {
-            throw new NotImplementedException();
+            // get handle to service
+            Service.ServiceController controller = new Service.ServiceController(_serviceName, _machineName);
+            //TODO ...
+
+            
+            return true;
         }
+
+
     }
 }
