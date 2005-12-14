96cf94b874948c7a4eb852205ebdc330e81d144e
dlrohm <dlrohm@299a232d-b705-0410-b782-f21f2f1e606a>
2005-12-14 07:23:06 +0000

    Added test for Date BuildType.
    git-svn-id: http://msbuildtasks.tigris.org/svn/msbuildtasks/trunk@61 299a232d-b705-0410-b782-f21f2f1e606a

--- Source/MSBuild.Community.Tasks.Tests/VersionTest.cs
+++ Source/MSBuild.Community.Tasks.Tests/VersionTest.cs
@@ -20,40 +20,57 @@ namespace MSBuild.Community.Tasks.Tests
             //
         }
 
-        [Test]
-        public void VersionExecute()
-        {
-            Version task = new Version();
-            task.BuildEngine = new MockBuild();
-            task.VersionFile = "number.txt";
-            Assert.IsTrue(task.Execute(), "Execute Failed");
+		[Test]
+		public void VersionExecute()
+		{
+			Version task = new Version();
+			task.BuildEngine = new MockBuild();
+			task.VersionFile = "number.txt";
+			Assert.IsTrue(task.Execute(), "Execute Failed");
 
-            Assert.AreEqual(1, task.Major);
-            Assert.AreEqual(0, task.Minor); 
-            Assert.AreEqual(0, task.Build);
-            Assert.AreEqual(0, task.Revision);
+			Assert.AreEqual(1, task.Major);
+			Assert.AreEqual(0, task.Minor);
+			Assert.AreEqual(0, task.Build);
+			Assert.AreEqual(0, task.Revision);
 
-            task = new Version();
-            task.BuildEngine = new MockBuild();
-            task.VersionFile = @"version.txt";
-            task.BuildType = "Increment";
-            task.RevisionType = "Increment";
-            Assert.IsTrue(task.Execute(), "Execute Failed");
+			task = new Version();
+			task.BuildEngine = new MockBuild();
+			task.VersionFile = @"version.txt";
+			task.BuildType = "Increment";
+			task.RevisionType = "Increment";
+			Assert.IsTrue(task.Execute(), "Execute Failed");
 
-            Assert.AreEqual(1, task.Major);
-            Assert.AreEqual(0, task.Minor);
+			Assert.AreEqual(1, task.Major);
+			Assert.AreEqual(0, task.Minor);
 
 
-            task = new Version();
-            task.BuildEngine = new MockBuild();
-            task.VersionFile = @"version.txt";
-            task.BuildType = "Automatic";
-            task.RevisionType = "Automatic";
-            Assert.IsTrue(task.Execute(), "Execute Failed");
+			task = new Version();
+			task.BuildEngine = new MockBuild();
+			task.VersionFile = @"version.txt";
+			task.BuildType = "Automatic";
+			task.RevisionType = "Automatic";
+			Assert.IsTrue(task.Execute(), "Execute Failed");
 
-            Assert.AreEqual(1, task.Major);
-            Assert.AreEqual(0, task.Minor); 
-            
-        }
+			Assert.AreEqual(1, task.Major);
+			Assert.AreEqual(0, task.Minor);
+
+			task = new Version();
+			task.BuildEngine = new MockBuild();
+			task.VersionFile = @"version.txt";
+			task.BuildType = "Date";
+			task.RevisionType = "Increment";
+			Assert.IsTrue(task.Execute(), "Execute Failed");
+
+			Assert.AreEqual(1, task.Major);
+			Assert.AreEqual(0, task.Minor);
+
+			DateTime dDate = DateTime.Now;
+			int _month = dDate.Month * 100;
+			int _day = dDate.Day;
+			int _year = (dDate.Year % 2000) * 10000;
+			int buildDate = _year + _month + _day;
+
+			Assert.AreEqual(buildDate, task.Build);
+		}
     }
 }
