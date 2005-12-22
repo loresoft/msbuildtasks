b79a0be0705b7009c7980bfb9158615cbeb3a841
dlrohm <dlrohm@299a232d-b705-0410-b782-f21f2f1e606a>
2005-12-22 09:39:43 +0000

    Updated to test new Path property for FileUpdate task.
    git-svn-id: http://msbuildtasks.tigris.org/svn/msbuildtasks/trunk@95 299a232d-b705-0410-b782-f21f2f1e606a

--- Source/MSBuild.Community.Tasks.Tests/FileUpdateTest.cs
+++ Source/MSBuild.Community.Tasks.Tests/FileUpdateTest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using NUnit.Framework;
 using Microsoft.Build.Utilities;
@@ -9,25 +10,51 @@ namespace MSBuild.Community.Tasks.Tests
     [TestFixture]
     public class FileUpdateTest
     {
-        [Test]
-        public void FileUpdateExecute()
-        {
-            VersionTest version = new VersionTest();
-            version.VersionExecute();
+		[TestFixtureSetUp]
+		public void FixtureInit()
+		{
+			// Create test files
+			File.WriteAllText(@".\number.txt", "1.0.0.0");
+			File.WriteAllText(@".\version.txt", "1.0.0.0");
+			File.WriteAllText(@".\pathtest.txt", "Today's date is: @DATE@." + Environment.NewLine +
+				"In case you didn't understand, today's date is: @DATE@!");
+		}
 
+		[TestFixtureTearDown]
+		public void FixtureDispose()
+		{
+			// Clean up test files
+			File.Delete(@".\number.txt");
+			File.Delete(@".\version.txt");
+			File.Delete(@".\pathtest.txt");
+		}
+
+        [Test]
+        public void TestFilesPropertyUpdate()
+        {
             FileUpdate task = new FileUpdate();
             task.BuildEngine = new MockBuild();
 
-            string[] files = new string[] {"version.txt", "number.txt"};
+            string[] files = new string[] {"number.txt", "version.txt"};
             TaskItem[] items = TaskUtility.StringArrayToItemArray(files);
 
             task.Files = items;
             task.Regex = @"(\d+)\.(\d+)\.(\d+)\.(\d+)";
             task.ReplacementText = "$1.$2.$3.123";
-            
-            Assert.IsTrue(task.Execute(), "Execute Failed");
+            Assert.IsTrue(task.Execute(), "Execute Failed!");
+		}
 
-            
-        }
+		[Test]
+		public void TestPathPropertyUpdate()
+		{
+			// Test inputting a path
+			FileUpdate task = new FileUpdate();
+			task.BuildEngine = new MockBuild();
+			task.Path = @".\";
+			task.Regex = @"@\w*?@";
+			task.ExcludeFileTypes = ".exe, .dll, .compiled, .jpg, .gif, .pdb";
+			task.ReplacementText = DateTime.Now.ToString();
+			Assert.IsTrue(task.Execute(), "Execute Failed!");
+		}
     }
 }
