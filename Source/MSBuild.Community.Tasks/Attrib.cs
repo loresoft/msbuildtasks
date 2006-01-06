671414c7eb49b8711adcd76c09a4689ea4760802
james1304 <james1304@299a232d-b705-0410-b782-f21f2f1e606a>
2006-01-06 10:10:11 +0000

    Added Attrib task
    git-svn-id: http://msbuildtasks.tigris.org/svn/msbuildtasks/trunk@98 299a232d-b705-0410-b782-f21f2f1e606a

---
+++ Source/MSBuild.Community.Tasks/Attrib.cs
@@ -0,0 +1,55 @@
+#region Copyright © 2005 Paul Welter. All rights reserved.
+/*
+Copyright © 2005 Paul Welter. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the author may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ * 
+ * James Higgs (james.higgs@interesource.com)
+*/
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace MSBuild.Community.Tasks
+{
+    /// <summary>
+    /// Changes the attributes of files and/or directories
+    /// </summary>
+    public class Attrib : Task
+    {
+        /// <summary>
+        /// Executes the task.
+        /// </summary>
+        /// <returns><see langword="true"/> if the task ran successfully; 
+        /// otherwise <see langword="false"/>.</returns>
+        public override bool Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
