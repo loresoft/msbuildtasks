717a8a49e08ab52a3f7bc28d3d8396d19a473c6a
pwelter34 <pwelter34@299a232d-b705-0410-b782-f21f2f1e606a>
2006-09-23 20:25:02 +0000

    finish GacUtil task
    git-svn-id: http://msbuildtasks.tigris.org/svn/msbuildtasks/trunk@256 299a232d-b705-0410-b782-f21f2f1e606a

--- Source/MSBuild.Community.Tasks/GacUtil.cs
+++ Source/MSBuild.Community.Tasks/GacUtil.cs
@@ -38,15 +38,40 @@ using Microsoft.Build.Framework;
 
 namespace MSBuild.Community.Tasks
 {
+    /// <summary>
+    /// The list of the commans available to the GacUtil Task
+    /// </summary>
     public enum GacUtilCommands
     {
+        /// <summary>Install the list of assemblies into the GAC.</summary>
         Install,
+        /// <summary>Uninstall the list of assembly names from the GAC.</summary>
         Uninstall,
     }
     
+    /// <summary>
+    /// MSBuild task to install and uninstall asseblies into the GAC
+    /// </summary>
+    /// <example>Install a dll into the GAC.
+    /// <code><![CDATA[
+    ///     <GacUtil 
+    ///         Command="Install" 
+    ///         Assemblies="MSBuild.Community.Tasks.dll" 
+    ///         Force="true" />
+    /// ]]></code>
+    /// </example>
+    /// <example>Uninstall a dll from the GAC.
+    /// <code><![CDATA[
+    ///     <GacUtil 
+    ///         Command="Uninstall" 
+    ///         Assemblies="MSBuild.Community.Tasks" 
+    ///         Force="true" />
+    /// ]]></code>
+    /// </example>
     public class GacUtil : ToolTask
     {
 
+        private string _assemblyListFile;
 
         #region Properties
         private GacUtilCommands _command = GacUtilCommands.Install;
@@ -59,7 +84,14 @@ namespace MSBuild.Community.Tasks
         public string Command
         {
             get { return _command.ToString(); }
-            set { _command = (GacUtilCommands)Enum.Parse(typeof(GacUtilCommands), value); }
+            set
+            {
+                if (Enum.IsDefined(typeof(GacUtilCommands), value))
+                    _command = (GacUtilCommands)Enum.Parse(typeof(GacUtilCommands), value);
+                else
+                    throw new ArgumentException(
+                        string.Format("The value '{0}' is not in the GacUtilCommans Enum.", value));
+            }
         }
 
         private bool _force;
@@ -74,31 +106,110 @@ namespace MSBuild.Community.Tasks
             set { _force = value; }
         }
 
-        private ITaskItem[] _assemblies;
-        
+        private string[] _assemblies;
+
 
         /// <summary>
-        /// Gets or sets the assemblies.
+        /// Gets or sets the assembly.
         /// </summary>
-        /// <value>The assemblies.</value>
-        [Required]
-        public ITaskItem[] Assemblies
+        /// <value>The assembly.</value>
+         [Required]
+        public string[] Assemblies
         {
             get { return _assemblies; }
             set { _assemblies = value; }
-        } 
-        #endregion
-		
-        protected override string GenerateFullPathToTool()
-        {
-            return Path.Combine(
-                ToolLocationHelper.GetPathToDotNetFrameworkSdk(TargetDotNetFrameworkVersion.Version20),
-                this.ToolName);
         }
 
+        #endregion
+
+        /// <summary>
+        /// Returns the fully qualified path to the executable file.
+        /// </summary>
+        /// <returns>
+        /// The fully qualified path to the executable file.
+        /// </returns>
+        protected override string GenerateFullPathToTool()
+        {
+            return ToolLocationHelper.GetPathToDotNetFrameworkSdkFile(
+                ToolName, TargetDotNetFrameworkVersion.Version20);
+        }
+
+        /// <summary>
+        /// Gets the name of the executable file to run.
+        /// </summary>
+        /// <value></value>
+        /// <returns>The name of the executable file to run.</returns>
         protected override string ToolName
         {
             get { return "gacutil.exe"; }
         }
+
+        /// <summary>
+        /// Logs the starting point of the run to all registered loggers.
+        /// </summary>
+        /// <param name="message">A descriptive message to provide loggers, usually the command line and switches.</param>
+        protected override void LogToolCommand(string message)
+        {
+            Log.LogCommandLine(MessageImportance.Low, message);
+        }
+        
+        /// <summary>
+        /// Gets the <see cref="T:Microsoft.Build.Framework.MessageImportance"></see> with which to log errors.
+        /// </summary>
+        /// <value></value>
+        /// <returns>The <see cref="T:Microsoft.Build.Framework.MessageImportance"></see> with which to log errors.</returns>
+        protected override MessageImportance StandardOutputLoggingImportance
+        {
+            get { return MessageImportance.Normal; }
+        }
+
+        /// <summary>
+        /// Returns a string value containing the command line arguments to pass directly to the executable file.
+        /// </summary>
+        /// <returns>
+        /// A string value containing the command line arguments to pass directly to the executable file.
+        /// </returns>
+        protected override string GenerateCommandLineCommands()
+        {          
+            CommandLineBuilder builder = new CommandLineBuilder();
+            builder.AppendSwitch("/nologo");
+            
+            if (_force)
+                builder.AppendSwitch("/f");
+
+            switch(_command)
+            {
+                case GacUtilCommands.Install:
+                    builder.AppendSwitch("/il");
+                    break;
+                case GacUtilCommands.Uninstall:
+                    builder.AppendSwitch("/ul");
+                    break;
+            }
+            builder.AppendFileNameIfNotNull(_assemblyListFile);
+            return builder.ToString();
+        }
+
+        /// <summary>
+        /// Runs the exectuable file with the specified task parameters.
+        /// </summary>
+        /// <returns>
+        /// true if the task runs successfully; otherwise, false.
+        /// </returns>
+        public override bool Execute()
+        {
+            // write asseblies to text file so we can use /il and /ul switch
+            _assemblyListFile = Path.GetTempFileName();
+            File.WriteAllLines(_assemblyListFile, _assemblies);
+            try
+            {
+                return base.Execute();
+            }
+            finally
+            {
+                File.Delete(_assemblyListFile);  // delete temp file
+            }            
+        }
+        
     }
 }
