18994210094666eb4e9824eb9877e156734d803b
joshuaflanagan <joshuaflanagan@299a232d-b705-0410-b782-f21f2f1e606a>
2006-10-14 19:08:12 +0000

    Starting to implement some Team Foundation Server Version Control tasks. TfsVersion is equivalent to the SvnVersion task. The tasks are intentionally designed to NOT required the TFS client on the developer's machine for compilation.
    git-svn-id: http://msbuildtasks.tigris.org/svn/msbuildtasks/trunk@260 299a232d-b705-0410-b782-f21f2f1e606a

---
+++ Source/MSBuild.Community.Tasks/Tfs/IServer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace MSBuild.Community.Tasks.Tfs
+{
+    /// <summary>
+    /// Describes the behavior of a Team Foundation Server
+    /// </summary>
+    public interface IServer
+    {
+        /// <summary>
+        /// Retrieves the latest changeset ID associated with a path
+        /// </summary>
+        /// <param name="server">The Team Foundation Server URL</param>
+        /// <param name="localPath">A path on the local filesystem</param>
+        /// <param name="credentials">Credentials used to authenticate against the serer</param>
+        /// <returns></returns>
+        int GetLatestChangesetId(string server, string localPath, System.Net.ICredentials credentials);
+    }
+}
