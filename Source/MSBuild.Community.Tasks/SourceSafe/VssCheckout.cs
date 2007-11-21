9823d8869ec2852f804429e03d724c8d4bc1256f
pcpatel <pcpatel@299a232d-b705-0410-b782-f21f2f1e606a>
2007-11-21 21:08:13 +0000

    Allowed for checkout of files using wildcard pattern. Example: $/Project/AssemblyInfo.*
    git-svn-id: http://msbuildtasks.tigris.org/svn/msbuildtasks/trunk@364 299a232d-b705-0410-b782-f21f2f1e606a

--- Source/MSBuild.Community.Tasks/SourceSafe/VssCheckout.cs
+++ Source/MSBuild.Community.Tasks/SourceSafe/VssCheckout.cs
@@ -31,6 +31,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 using Microsoft.Build.Utilities;
 using Microsoft.Build.Framework;
 using Microsoft.VisualStudio.SourceSafe.Interop;
@@ -88,27 +89,38 @@ namespace MSBuild.Community.Tasks.SourceSafe
         {
             try
             {
-                FileInfo localPath = new FileInfo(_localPath);
+                string searchPattern = "";
+                if (Path.Contains("*"))
+                {
+                    // If the path has a wildcard character (*), we use the last part (or the search pattern part) 
+                    // of the path to selectively checkout files based on wildcard search using regular expression. 
+                    // In order to achieve this we strip out the search pattern part of the path, if the path has 
+                    // a wildcard character in it, and use it for the pattern in our regular expression search, but 
+                    // only if search pattern part contains a wildcard character.
+                    // Example: $/Project/AssemblyInfo.*
+                    Path = Path.TrimEnd(new char[] { '/' });
+                    searchPattern = Path.Substring(Path.LastIndexOf('/') + 1);
+                    searchPattern = searchPattern.Replace(".", "\\.").Replace("*", ".*");
+                    if (searchPattern.Contains("*"))
+                    {
+                        // The search pattern part of the path is valid, so we'll remove the search pattern from
+                        // the path. This way our starting point will be a project and not a file.
+                        Path = Path.Substring(0, Path.LastIndexOf('/'));
+                    }
+                    else
+                    {
+                        // The wildcard is not in the search pattern part of the path but elsewhere, so we'll
+                        // try to checkout the provided path as-is. 
+                        searchPattern = "";
+                    }
+                }
+
                 ConnectToDatabase();
 
                 int flags = (Recursive ? Convert.ToInt32(RecursiveFlag) : 0) |
                     (Writable ? Convert.ToInt32(VSSFlags.VSSFLAG_USERRONO) : Convert.ToInt32(VSSFlags.VSSFLAG_USERROYES));
-                
-                //TODO: timestamp stuff
 
-                switch (Item.Type)
-                {
-                    case (int)VSSItemType.VSSITEM_PROJECT:
-                        Item.Checkout("", localPath.FullName, flags);
-                        break;
-                    case (int)VSSItemType.VSSITEM_FILE:
-                        string filePath = System.IO.Path.Combine(
-                            localPath.FullName,
-                            Item.Name
-                        );
-                        Item.Checkout("", filePath, flags);
-                        break;
-                }
+                Checkout(Item, new FileInfo(_localPath), flags, searchPattern);
 
                 Log.LogMessage(MessageImportance.Normal, "Checked out '{0}'.", Path);
                 return true;
@@ -117,6 +129,54 @@ namespace MSBuild.Community.Tasks.SourceSafe
             {
                 LogErrorFromException(e);
                 return false;
+            }
+        }
+
+        private void Checkout(IVSSItem checkoutItem, FileInfo localPath, int flags, string pattern)
+        {
+            //TODO: timestamp stuff
+
+            switch (checkoutItem.Type)
+            {
+                case (int)VSSItemType.VSSITEM_PROJECT:
+                    // Item is a project.
+                    if (string.IsNullOrEmpty(pattern))
+                    {
+                        // In the absence of a pattern, we'll checkout the entire project.
+                        checkoutItem.Checkout("", localPath.FullName, flags);
+                    }
+                    else
+                    {
+                        // If a pattern is provided, we process all subitems in the project.
+                        foreach (IVSSItem subItem in checkoutItem.get_Items(false))
+                        {
+                            switch (subItem.Type)
+                            {
+                                case (int)VSSItemType.VSSITEM_PROJECT:
+                                    // Subitem is a project.
+                                    if (Recursive)
+                                    {
+                                        // We'll recursively checkout matching files in this project.
+                                        Checkout(subItem, new FileInfo(System.IO.Path.Combine(localPath.FullName, subItem.Name)), flags, pattern);
+                                    }
+                                    break;
+                                case (int)VSSItemType.VSSITEM_FILE:
+                                    // Subitem is a file.
+                                    if (Regex.IsMatch(subItem.Name, pattern))
+                                    {
+                                        // We'll checkout this file since it matches the search pattern.
+                                        Checkout(subItem, localPath, flags, "");
+                                    }
+                                    break;
+                            }
+                        }
+                    }
+                    break;
+                case (int)VSSItemType.VSSITEM_FILE:
+                    // Item is a file.
+                    string filePath = System.IO.Path.Combine(localPath.FullName, checkoutItem.Name);
+                    checkoutItem.Checkout("", filePath, flags);
+                    break;
             }
         }
     }
