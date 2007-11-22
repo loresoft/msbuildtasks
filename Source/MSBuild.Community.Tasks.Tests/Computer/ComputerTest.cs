4aa705a153a5ff09661ef61226ff97d113d05395
pcpatel <pcpatel@299a232d-b705-0410-b782-f21f2f1e606a>
2007-11-22 18:22:00 +0000

    Added new Computer task which provides information about the build computer.
    git-svn-id: http://msbuildtasks.tigris.org/svn/msbuildtasks/trunk@368 299a232d-b705-0410-b782-f21f2f1e606a

---
+++ Source/MSBuild.Community.Tasks.Tests/Computer/ComputerTest.cs
@@ -0,0 +1,54 @@
+#region Copyright © 2007 Pinal Patel. All rights reserved.
+/*
+Copyright © 2007 Pinal Patel. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the author may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+*/
+#endregion
+
+// $Id$
+
+using System;
+using System.Text;
+using NUnit.Framework;
+
+namespace MSBuild.Community.Tasks.Tests
+{
+    [TestFixture()]
+    public class ComputerTest
+    {
+        [Test(Description = "Get information about the build computer")]
+        public void ComputerExecute()
+        {
+            Computer task = new Computer();
+            task.BuildEngine = new MockBuild();
+
+            Assert.IsTrue(task.Execute(), "Execute Failed");
+            Assert.IsNotEmpty(task.Name, "Name should not be empty");
+            Assert.IsNotEmpty(task.IPAddress, "IPAddress should not be empty");
+            Assert.IsNotEmpty(task.OSPlatform, "OSPlatform should not be empty");
+            Assert.IsNotEmpty(task.OSVersion, "OSVersion should not be empty");
+        }
+    }
+}
