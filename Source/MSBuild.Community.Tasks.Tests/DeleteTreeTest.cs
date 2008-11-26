5264458f84f55d93955d97b5fa9da30149b01d66
pwelter34 <pwelter34@299a232d-b705-0410-b782-f21f2f1e606a>
2008-11-26 02:20:00 +0000

    - add DeleteTree task
    git-svn-id: http://msbuildtasks.tigris.org/svn/msbuildtasks/trunk@431 299a232d-b705-0410-b782-f21f2f1e606a

---
+++ Source/MSBuild.Community.Tasks.Tests/DeleteTreeTest.cs
@@ -0,0 +1,125 @@
+using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using NUnit.Framework;
+
+namespace MSBuild.Community.Tasks.Tests
+{
+    [TestFixture]
+    public class DeleteTreeTest
+    {
+        [Test]
+        public void GetDirectories()
+        {
+            string root = Path.GetFullPath(@"..\..\..\");
+
+            string[] directories = Directory.GetDirectories(root, "*", SearchOption.AllDirectories);
+        }
+
+        [Test]
+        public void ExecuteInnerAndTrailingRecursive()
+        {
+            DeleteTree task = new DeleteTree();
+            task.BuildEngine = new MockBuild();
+            task.Directories = new ITaskItem[] { new TaskItem(@"..\..\..\**\obj\**")};
+            bool result = task.Execute();
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void ExecuteInnerRecursive()
+        {
+            DeleteTree task = new DeleteTree();
+            task.BuildEngine = new MockBuild();
+            task.Directories = new ITaskItem[] { new TaskItem(@"..\..\..\**\obj") };
+            bool result = task.Execute();
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void ExecuteItemWithTrailingSeparator()
+        {
+            DeleteTree task = new DeleteTree();
+            task.BuildEngine = new MockBuild();
+            task.Directories = new ITaskItem[] { new TaskItem(@"..\..\..\**\obj\") };
+            bool result = task.Execute();
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void ExecuteMultipleItems()
+        {
+            DeleteTree task = new DeleteTree();
+            task.BuildEngine = new MockBuild();
+            task.Directories = new ITaskItem[]
+                                   {
+                                       new TaskItem(@"..\..\..\**\obj"), 
+                                       new TaskItem(@"..\..\..\**\bin")
+                                   };
+
+            bool result = task.Execute();
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void ExecuteWildCard()
+        {
+            DeleteTree task = new DeleteTree();
+            task.BuildEngine = new MockBuild();
+            task.Directories = new ITaskItem[]
+                                   {
+                                       new TaskItem(@"..\..\..\MSBuild.Community.*\**\obj"), 
+                                       new TaskItem(@"..\..\..\**\b?n")
+                                   };
+
+            bool result = task.Execute();
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void MatchDirectoriesNoWildCards()
+        {
+            var path = DeleteTree.MatchDirectories(@"..\..\..");
+            Assert.AreEqual(1, path.Count);
+        }
+
+        [Test]
+        public void MatchDirectoriesInnerRelative()
+        {
+            var path = DeleteTree.MatchDirectories(@"..\..\..\**\obj");
+            Assert.Greater(path.Count, 0);
+        }
+
+        [Test]
+        public void MatchDirectoriesInnerAndOuterRelative()
+        {
+            var path = DeleteTree.MatchDirectories(@"..\..\..\**\obj\**");
+            Assert.Greater(path.Count, 0);
+        }
+
+
+        [Test]
+        public void MatchDirectoriesRelative()
+        {
+            var path = DeleteTree.MatchDirectories(@"..\..\..\**");
+            Assert.Greater(path.Count, 0);
+        }
+
+        [Test]
+        public void MatchDirectoriesRelativeWildCard()
+        {
+            var path = DeleteTree.MatchDirectories(@"..\..\..\MSBuild.Community.*\**\obj");
+            Assert.Greater(path.Count, 0);
+
+            path = DeleteTree.MatchDirectories(@"..\..\..\MSBuild.*.Tests\**\obj\**");
+            Assert.Greater(path.Count, 0);
+        }
+
+        
+    }
+}
