81c02745c496e97f6799a07f5120086702ff3940
pwelter34 <pwelter34@299a232d-b705-0410-b782-f21f2f1e606a>
2011-06-27 16:13:40 +0000

    - add NuGet - add TfsClient
    git-svn-id: http://msbuildtasks.tigris.org/svn/msbuildtasks/trunk@513 299a232d-b705-0410-b782-f21f2f1e606a

---
+++ Source/MSBuild.Community.Tasks/NuGet.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.IO;
+using System.Resources;
+using System.Text;
+using System.Xml;
+using System.Xml.XPath;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace MSBuild.Community.Tasks
+{
+    /// <summary>
+    /// Creates a NuGet package based on the specified manifest (Nuspec) file.
+    /// Can set the version number in the manifest prior to building the package.
+    /// </summary>
+    public class NuGet : ToolTask
+    {
+        /// <summary>
+        /// The location of the manifest (Nuspec) file to create a package.
+        /// </summary>
+        /// <value>The manifest.</value>
+        [Required]
+        public ITaskItem Manifest { get; set; }
+
+        /// <summary>
+        /// Specifies the directory for the created NuGet package.
+        /// </summary>
+        /// <value>The output directory.</value>
+        public string OutputDirectory { get; set; }
+
+        /// <summary>
+        /// The Base Path of the files defined in the nuspec file.
+        /// </summary>
+        /// <value>The base path.</value>
+        public string BasePath { get; set; }
+
+        /// <summary>
+        /// Shows verbose output for package building.
+        /// </summary>
+        /// <value><c>true</c> if verbose; otherwise, <c>false</c>.</value>
+        public bool Verbose { get; set; }
+
+        /// <summary>
+        /// Sets the version of the package in the manifest (Nuspec) file.
+        /// The version must specify at least two places "X.X".
+        /// </summary>
+        /// <value>The version to set in the manifest.</value>
+        public string Version { get; set; }
+
+        /// <summary>
+        /// Gets or sets the working directory.
+        /// </summary>
+        /// <value>The working directory.</value>
+        /// <returns>
+        /// The directory in which to run the executable file, or a null reference (Nothing in Visual Basic) if the executable file should be run in the current directory.
+        /// </returns>
+        public string WorkingDirectory { get; set; }
+
+        /// <summary>
+        /// Runs the exectuable file with the specified task parameters.
+        /// </summary>
+        /// <returns>
+        /// true if the task runs successfully; otherwise, false.
+        /// </returns>
+        public override bool Execute()
+        {
+            if (!string.IsNullOrEmpty(Version))
+                UpdateVersion();
+
+            return base.Execute();
+        }
+
+        private void UpdateVersion()
+        {
+
+            var document = new XmlDocument();
+            document.Load(Manifest.ItemSpec);
+            
+            var navigator = document.CreateNavigator();
+            var manager = new XmlNamespaceManager(navigator.NameTable);
+            manager.AddNamespace("d", "http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd");
+            
+            var expression = XPathExpression.Compile("/d:package/d:metadata/d:version", manager);
+            var iterator = navigator.Select(expression);
+
+            while (iterator.MoveNext())
+                if (iterator.Current != null)
+                    iterator.Current.SetValue(Version);
+
+            using (var writer = new XmlTextWriter(Manifest.ItemSpec, Encoding.UTF8))
+            {
+                writer.Formatting = Formatting.Indented;
+                document.Save(writer);
+                writer.Close();
+            }
+
+        }
+        
+        /// <summary>
+        /// Returns a string value containing the command line arguments to pass directly to the executable file.
+        /// </summary>
+        /// <returns>
+        /// A string value containing the command line arguments to pass directly to the executable file.
+        /// </returns>
+        protected override string GenerateCommandLineCommands()
+        {
+            CommandLineBuilder builder = new CommandLineBuilder();
+            builder.AppendSwitch("package");
+            builder.AppendFileNameIfNotNull(Manifest);
+            builder.AppendSwitchIfNotNull("-OutputDirectory", OutputDirectory);
+            builder.AppendSwitchIfNotNull("-BasePath", BasePath);
+            if (Verbose)
+                builder.AppendSwitch("-Verbose");
+
+            return builder.ToString();
+        }
+
+        /// <summary>
+        /// Returns the fully qualified path to the executable file.
+        /// </summary>
+        /// <returns>
+        /// The fully qualified path to the executable file.
+        /// </returns>
+        protected override string GenerateFullPathToTool()
+        {
+            if (string.IsNullOrEmpty(ToolPath))
+                return ToolName;
+
+            return Path.Combine(ToolPath, ToolName);
+        }
+
+        /// <summary>
+        /// Gets the name of the executable file to run.
+        /// </summary>
+        /// <value></value>
+        /// <returns>The name of the executable file to run.</returns>
+        protected override string ToolName
+        {
+            get { return "NuGet.exe"; }
+        }
+
+        /// <summary>
+        /// Returns the directory in which to run the executable file.
+        /// </summary>
+        /// <returns>
+        /// The directory in which to run the executable file, or a null reference (Nothing in Visual Basic) if the executable file should be run in the current directory.
+        /// </returns>
+        protected override string GetWorkingDirectory()
+        {
+            return string.IsNullOrEmpty(WorkingDirectory) 
+                ? base.GetWorkingDirectory() 
+                : WorkingDirectory;
+        }
+    }
+}
---
+++ Source/MSBuild.Community.Tasks/Tfs/TfsClient.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace MSBuild.Community.Tasks.Tfs
+{
+    public class TfsClient : ToolTask
+    {
+
+        public string Command { get; set; }
+
+        public ITaskItem[] Files { get; set; }
+
+        public bool Recursive { get; set; }
+        public bool All { get; set; }
+        public bool Overwrite { get; set; }
+        public bool Override { get; set; }
+        public bool Force { get; set; }
+        public bool Preview { get; set; }
+        public bool Remap { get; set; }
+        public bool Silent { get; set; }
+        public bool Saved { get; set; }
+        public bool Validate { get; set; }
+        public bool Bypass { get; set; }
+
+        public string Comment { get; set; }
+        public string Version { get; set; }
+        public string Lock { get; set; }
+        public string Type { get; set; }
+        public string Author { get; set; }
+        public string Notes { get; set; }
+        public string Format { get; set; }
+        public string Collection { get; set; }
+
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string WorkspaceName { get; set; }
+        public string WorkspaceOwner { get; set; }
+        public string ShelveSetName { get; set; }
+        public string ShelveSetOwner { get; set; }
+        
+        
+        private string FindToolPath(string toolName)
+        {
+            return string.Empty;
+        }
+
+        protected virtual void GenerateCommand(CommandLineBuilder builder)
+        {
+            builder.AppendSwitch(Command);
+            builder.AppendFileNamesIfNotNull(Files, " ");
+        }
+
+        protected virtual void GenerateArguments(CommandLineBuilder builder)
+        {
+            builder.AppendSwitch("/noprompt");
+
+            builder.AppendSwitchIfNotNull("/comment:", Comment);
+            builder.AppendSwitchIfNotNull("/version:", Version);
+            builder.AppendSwitchIfNotNull("/lock:", Lock);
+            builder.AppendSwitchIfNotNull("/type:", Type);
+            builder.AppendSwitchIfNotNull("/author:", Author);
+            builder.AppendSwitchIfNotNull("/notes:", Notes);
+            builder.AppendSwitchIfNotNull("/format:", Format);
+            builder.AppendSwitchIfNotNull("/collection:", Collection);
+
+            if (Recursive)
+                builder.AppendSwitch("/recursive");
+            if (All)
+                builder.AppendSwitch("/all");
+            if (Override)
+                builder.AppendSwitch("/override");
+            if (Overwrite)
+                builder.AppendSwitch("/overwrite");
+            if (Force)
+                builder.AppendSwitch("/force");
+            if (Preview)
+                builder.AppendSwitch("/preview");
+            if (Remap)
+                builder.AppendSwitch("/remap");
+            if (Silent)
+                builder.AppendSwitch("/silent");
+            if (Saved)
+                builder.AppendSwitch("/saved");
+            if (Validate)
+                builder.AppendSwitch("/validate");
+            if (Bypass)
+                builder.AppendSwitch("/bypass");
+
+            if (!string.IsNullOrEmpty(UserName))
+            {
+                string login = "/login:" + UserName;
+                if (!string.IsNullOrEmpty(Password))
+                    login += "," + Password;
+
+                builder.AppendSwitch(login);
+            }
+            
+            if (!string.IsNullOrEmpty(WorkspaceName))
+            {
+                string workspace = "/workspace:" + WorkspaceName;
+                if (!string.IsNullOrEmpty(WorkspaceOwner))
+                    workspace += "," + WorkspaceOwner;
+
+                builder.AppendSwitch(workspace);
+            }
+
+            if (!string.IsNullOrEmpty(ShelveSetName))
+            {
+                string shelveset = "/shelveset:" + ShelveSetName;
+                if (!string.IsNullOrEmpty(ShelveSetOwner))
+                    shelveset += "," + ShelveSetOwner;
+
+                builder.AppendSwitch(shelveset);
+            }
+        }
+
+        /// <summary>
+        /// Returns the fully qualified path to the executable file.
+        /// </summary>
+        /// <returns>
+        /// The fully qualified path to the executable file.
+        /// </returns>
+        protected override string GenerateFullPathToTool()
+        {
+            if (string.IsNullOrEmpty(ToolPath))
+                ToolPath = FindToolPath(ToolName);
+
+            return Path.Combine(ToolPath, ToolName);
+        }
+
+        /// <summary>
+        /// Logs the starting point of the run to all registered loggers.
+        /// </summary>
+        /// <param name="message">A descriptive message to provide loggers, usually the command line and switches.</param>
+        protected override void LogToolCommand(string message)
+        {
+            Log.LogCommandLine(MessageImportance.Low, message);
+        }
+
+        /// <summary>
+        /// Gets the <see cref="T:Microsoft.Build.Framework.MessageImportance"></see> with which to log errors.
+        /// </summary>
+        /// <value></value>
+        /// <returns>The <see cref="T:Microsoft.Build.Framework.MessageImportance"></see> with which to log errors.</returns>
+        protected override MessageImportance StandardOutputLoggingImportance
+        {
+            get { return MessageImportance.Normal; }
+        }
+
+        /// <summary>
+        /// Gets the name of the executable file to run.
+        /// </summary>
+        /// <returns>
+        /// The name of the executable file to run.
+        /// </returns>
+        protected override string ToolName
+        {
+            get { return "tf.exe"; }
+        }
+
+        /// <summary>
+        /// Returns a string value containing the command line arguments to pass directly to the executable file.
+        /// </summary>
+        /// <returns>
+        /// A string value containing the command line arguments to pass directly to the executable file.
+        /// </returns>
+        protected override string GenerateCommandLineCommands()
+        {
+            var commandLine = new CommandLineBuilder();
+            GenerateCommand(commandLine);
+            GenerateArguments(commandLine);
+
+            return commandLine.ToString();
+        }
+
+        /// <summary>
+        /// Indicates whether all task paratmeters are valid.
+        /// </summary>
+        /// <returns>
+        /// true if all task parameters are valid; otherwise, false.
+        /// </returns>
+        protected override bool ValidateParameters()
+        {
+            if (string.IsNullOrEmpty(Command))
+            {
+                Log.LogError(Properties.Resources.ParameterRequired, "TfsClient", "Command");
+                return false;
+            }
+            return base.ValidateParameters();
+        }
+    }
+}
