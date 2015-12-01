b7a6dcfab6d521ad5a1b022b8d433ddd205c9a5b
Vitaliy Pfau <vitaly.pfau@gmail.com>
2015-12-01 00:22:09 -0500

    DependencyGraph changes: - add project references - new IsIncludeProjectDependecies property - Filters property was removed; Instead that there were added exclude filters for input files (Exclude), referenced assemblies (ExcludeReferences), referenced projects (ExcludeProjectReferences) - English localization - minor refactoring on exist code - example

--- Source/MSBuild.Community.Tasks/DependencyGraph/DependencyGraph.cs
+++ Source/MSBuild.Community.Tasks/DependencyGraph/DependencyGraph.cs
@@ -9,45 +9,96 @@ using System.Text.RegularExpressions;
 
 namespace MSBuild.Community.Tasks.DependencyGraph
 {
-
     /// <summary>
     /// Reads a set of project files (.csproj, .vbproj) in InputFiles and generate a GraphViz style syntax.
     /// You can paste the result of the graphs in places like http://graphviz-dev.appspot.com/ to see your chart or
     /// run the file using the GraphViz tool http://www.graphviz.org/
     /// </summary>
+    /// <example>
+    /// <code><![CDATA[
+    /// 	<ItemGroup>
+	///         <Dependency Include="Trader.csproj" />
+	///     </ItemGroup>
+    ///
+	///     <Target Name="Default">
+    ///		    <DependencyGraph InputFiles="@(Dependency)" IsIncludeProjectDependecies="true" ExcludeProjectReferences="API;UI" />	<!-- API and UI project references will be excluded -->
+    ///     </Target>
+    ///
+    /// ]]></code></example>
+    /// <para>Result:</para>
+    /// <para><![CDATA[  digraph {
+    ///    "Trader" -> "System";
+    ///    "Trader" -> "System.configuration";
+    ///    "Trader" -> "System.Data";
+    ///    "Trader" -> "System.Deployment";
+    ///    "Trader" -> "System.Design";
+    ///    "Trader" -> "System.Drawing";
+    ///    "Trader" -> "System.Drawing.Design";
+    ///    "Trader" -> "System.EnterpriseServices";
+    ///    "Trader" -> "System.Web";
+    ///    "Trader" -> "System.Web.Services";
+    ///    "Trader" -> "System.Windows.Forms";
+    ///    "Trader" -> "System.Xml";
+    ///    "Trader" -> "Project References";
+    ///            "Project References" -> "CommLib";
+    ///            "Project References" -> "DdeApi";</para>
+    ///  ]]></para>
+    /// <para>
+    ///     Other attributes:
+    /// <list type="table">
+    /// <item>
+    ///     <term>Exclude</term>
+    ///     <description>filter input files</description>
+    /// </item>
+    ///  <item>
+    ///     <term>ExcludeReferences</term>
+    ///     <description>filter referenced assemblies</description>
+    /// </item>
+    /// </list>
+    /// </para>
     public class DependencyGraph : Task
     {
+        private static readonly Regex[] EmptyRegexBuffer = new Regex[0];
 
-        /// <summary>
-        /// Project files to parse
-        /// </summary>
+        private readonly Dictionary<string, List<BaseReference>> _dependencies = new Dictionary<string, List<BaseReference>>();
+        private Regex[] _excludeRegex;
+        private Regex[] _excludeReferencesRegex;
+        private Regex[] _excludeProjectReferencesRegex;
+
+        /// <summary> Project files to parse </summary>
         [Required]
-        public ITaskItem[] InputFiles
+        public ITaskItem[] InputFiles { get; set; }
+
+        /// <summary>A set of regular expression to filter the input files</summary>
+        public ITaskItem[] Exclude { get; set; }
+
+        private Regex[] ExcludeRegex
         {
-            get;
-            set;
+            get { return MakeFilterRegex(ref _excludeRegex, Exclude); }
         }
 
-        /// <summary>
-        /// A set of regular expression to filter the input files.
-        /// </summary>
-        public ITaskItem[] Filters
+        /// <summary>A set of regular expression to filter the referenced assemblies</summary>
+        public ITaskItem[] ExcludeReferences { get; set; }
+
+        private Regex[] ExcludeReferencesRegex
         {
-            get;
-            set;
+            get { return MakeFilterRegex(ref _excludeReferencesRegex, ExcludeReferences); }
         }
 
-        /// <summary>
-        /// FileName to output results
-        /// </summary>        
+        /// <summary>A set of regular expression to filter the referenced projects</summary>
+        public ITaskItem[] ExcludeProjectReferences { get; set; }
+
+        private Regex[] ExcludeProjectReferencesRegex
+        {
+            get { return MakeFilterRegex(ref _excludeProjectReferencesRegex, ExcludeProjectReferences); }
+        }
+
+        /// <summary>includes project dependencies to output</summary>
+        public bool IsIncludeProjectDependecies { get; set; }
+
+        /// <summary>FileName to output results</summary>        
         public string OutputFile { get; set; }
 
-        private Dictionary<string, List<string>> dependencies = new Dictionary<string, List<string>>();
-
-        /// <summary>
-        /// Runs the task.
-        /// </summary>
-        /// <returns></returns>
         public override bool Execute()
         {
             if (InputFiles == null)
@@ -57,35 +108,82 @@ namespace MSBuild.Community.Tasks.DependencyGraph
             }
 
             foreach (ITaskItem file in InputFiles)
-            {
-                var fullPath = file.GetMetadata("FullPath");
-                Log.LogMessage("Parsing " + fullPath);
-                FileStream fs = new FileStream(fullPath, FileMode.Open, FileAccess.Read);
-                using (fs)
-                {
-                    ProjectFileParser p = new ProjectFileParser(fs);
-                    if (!Filtered(p.GetAssemblyName()))
-                    {
-                        if (String.IsNullOrEmpty(p.GetAssemblyName()))
-                            Log.LogWarning("Não foi possível identificar AssemblyName para: " + file.GetMetadata("FullPath"));
-
-                        foreach (ProjectReference r in p.GetReferences())
-                        {
-                            if (String.IsNullOrEmpty(p.GetAssemblyNameFromFullName(r.Include)))
-                                Log.LogWarning("Não foi possível identificar referência encontrada em : " + file.GetMetadata("FullPath"));
-
-                            if (!Filtered(p.GetAssemblyNameFromFullName(r.Include)))
-                                AddDependency(p.GetAssemblyName(), p.GetAssemblyNameFromFullName(r.Include));
-                        }
-                    }
-                }
-            }
+                ProcessProject(file.GetMetadata("FullPath"));
 
             Stream s = GenerateGraphVizOutput();
             LogToConsole(s);
             LogToFile(s);
 
             return true;
+        }
+
+        private void ProcessProject(string fullFileName)
+        {
+            Log.LogMessage("Parsing " + fullFileName);
+            FileStream fs = new FileStream(fullFileName, FileMode.Open, FileAccess.Read);
+            using (fs)
+            {
+                ProjectFileParser parser = new ProjectFileParser(fs);
+                var assemblyName = parser.GetAssemblyName();
+
+                if (!IsApplicable(ExcludeRegex, assemblyName))
+                    return;
+
+                AddApplicableReference(assemblyName, ExcludeReferencesRegex, parser.GetAssemblyReferences());
+                if (IsIncludeProjectDependecies)
+                    AddApplicableReference(assemblyName, ExcludeProjectReferencesRegex, parser.GetProjectReferences());
+            }
+        }
+
+        private void AddApplicableReference(string projectName, Regex[] filters, IEnumerable<BaseReference> references)
+        {
+            foreach (BaseReference reference in references.Where(reference => IsApplicable(filters, reference.DisplayName)))
+                AddDependency(projectName, reference);
+        }
+
+        public static void Test()
+        {
+            var rex = new Regex("System");
+            Console.WriteLine(rex.IsMatch("Ionic.Zip.Reduced.dll"));
+        }
+
+        private void AddDependency(string projectName, BaseReference baseReference)
+        {
+            List<BaseReference> list;
+            if (!_dependencies.TryGetValue(projectName, out list))
+            {
+                list = new List<BaseReference>();
+                _dependencies.Add(projectName, list);
+            }
+            list.Add(baseReference);
+        }
+
+        private Stream GenerateGraphVizOutput()
+        {
+            MemoryStream ms = new MemoryStream();
+            StreamWriter sw = new StreamWriter(ms);
+
+            sw.WriteLine("digraph {");
+
+            foreach (KeyValuePair<string, List<BaseReference>> p in _dependencies)
+            {
+                foreach (var dep in p.Value.OfType<AssemblyReference>())
+                    sw.WriteLine("\t\"{0}\" -> \"{1}\";", p.Key, dep);
+
+                if (IsIncludeProjectDependecies && p.Value.Any())
+                {
+                    const string references = "Project References";
+                    sw.WriteLine("\t\"{0}\" -> \"{1}\";", p.Key, references);
+                    foreach (var dep in p.Value.OfType<ProjectReference>())
+                        sw.WriteLine("\t\t\"{0}\" -> \"{1}\";", references, dep);
+                }
+            }
+
+            sw.WriteLine("}");
+            sw.Flush();
+
+            ms.Seek(0, SeekOrigin.Begin);
+            return ms;
         }
 
         private void LogToFile(Stream s)
@@ -96,7 +194,7 @@ namespace MSBuild.Community.Tasks.DependencyGraph
             s.Seek(0, SeekOrigin.Begin);
             FileStream fs = new FileStream(OutputFile, FileMode.Create, FileAccess.Write);
             using (fs)
-                CopyStream(s, fs);                      
+                CopyStream(s, fs);
         }
 
         private void CopyStream(Stream input, Stream output)
@@ -116,45 +214,18 @@ namespace MSBuild.Community.Tasks.DependencyGraph
             Log.LogMessage(rd.ReadToEnd());
         }
 
-        private Stream GenerateGraphVizOutput()
+        private static Regex[] MakeFilterRegex(ref Regex[] buffer, IEnumerable<ITaskItem> items)
         {
-            MemoryStream ms = new MemoryStream();
-            StreamWriter sw = new StreamWriter(ms);
-
-            sw.WriteLine("digraph {");
-            foreach (KeyValuePair<string, List<string>> p in this.dependencies)
-                foreach (string dep in p.Value)
-                    sw.WriteLine("\t\"{0}\" -> \"{1}\";", p.Key, dep);
-            sw.WriteLine("}");
-            sw.Flush();
-            
-            ms.Seek(0, SeekOrigin.Begin);
-            return ms;
+            return 
+                buffer 
+                ?? (buffer = items == null
+                    ? EmptyRegexBuffer
+                    : items.Select(filter => new Regex(filter.ItemSpec)).ToArray());
         }
 
-        private void AddDependency(string projectName, string referenceName)
+        private static bool IsApplicable(Regex[] filters, string name)
         {
-            if (!dependencies.ContainsKey(projectName))
-                dependencies.Add(projectName, new List<string>());
-
-            dependencies[projectName].Add(referenceName);
+            return filters.Length == 0 || filters.All(filter => !filter.IsMatch(name));
         }
-
-        private bool Filtered(string identifier)
-        {
-            if (Filters == null || Filters.Length <= 0)
-                return false;
-
-            bool filtered = false;
-            foreach (ITaskItem filter in Filters)
-            {
-                Regex r = new Regex(filter.ItemSpec);
-                filtered = r.IsMatch(identifier);
-                if (filtered)
-                    break;
-            }
-            return filtered;
-        }
-
     }
 }
---
+++ Source/MSBuild.Community.Tasks/DependencyGraph/Reference.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MSBuild.Community.Tasks.DependencyGraph
+{
+    /// <summary>
+    /// Base class for all references 
+    /// </summary>
+    public class BaseReference
+    {
+        /// <summary>
+        /// A name for display in a graph
+        /// </summary>
+        public string DisplayName { get; private set; }
+
+        /// <summary>
+        /// Creates new instance
+        /// </summary>
+        /// <param name="displayName">A name for display in a graph</param>
+        protected BaseReference(string displayName)
+        {
+            DisplayName = displayName;
+        }
+
+        public override string ToString()
+        {
+            return DisplayName;
+        }
+    }
+
+    /// <summary>
+    /// Represents an assembly reference inside a project file
+    /// </summary>
+    public class AssemblyReference : BaseReference
+    {
+        /// <summary>
+        /// Name of the assembly of an assembly reference
+        /// </summary>
+        public string Include { get; private set; }
+
+        /// <summary>
+        /// HintPath, or relative path to file in an assembly reference
+        /// </summary>
+        public string HintPath { get; private set; }
+
+        /// <summary>
+        /// Creates new instance
+        /// </summary>
+        /// <param name="include">The name of the assembly reference</param>
+        /// <param name="hintPath">The hint path, if aplicable</param>
+        public AssemblyReference(string include, string hintPath)
+            : base(MakeDisplayName(include))
+        {
+            Include = include;
+            HintPath = hintPath;
+        }
+
+        private static string MakeDisplayName(string include)
+        {
+            var result = ProjectFileParser.GetAssemblyNameFromFullName(include);
+            return string.IsNullOrEmpty(result) ? include : result;
+        }
+    }
+
+    /// <summary>
+    /// Represents a project reference inside a project file
+    /// </summary>
+    public class ProjectReference : BaseReference
+    {
+        /// <summary> 
+        /// Path to a project file of reference 
+        /// </summary>
+        public string Include { get; private set; }
+
+        /// <summary>
+        /// GUID of referenced project
+        /// </summary>
+        public string Project { get; private set; }
+
+        /// <summary>
+        /// Name of referenced project
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Creates new instance
+        /// </summary>
+        /// <param name="include">The name of the assembly reference</param>
+        /// <param name="project">GUID of referenced project</param>
+        /// <param name="name">Name of referenced project</param>
+        public ProjectReference(string include, string project, string name)
+            : base (name)
+        {
+            Include = include;
+            Project = project;
+            Name = name;
+        }
+    }
+}
