bf7fc574f7e4b37a8be35707d8e1dd65a3c0cbcf
Vitaliy Pfau <vitaly.pfau@gmail.com>
2015-12-07 23:05:21 -0500

    DependencyGraph changes: - Recursive project dependencies - "Project References" node has been replaced with subgraph; Now Assembly references are oval, project references are box

--- Source/MSBuild.Community.Tasks/DependencyGraph/Reference.cs
+++ Source/MSBuild.Community.Tasks/DependencyGraph/Reference.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace MSBuild.Community.Tasks.DependencyGraph
@@ -13,7 +15,7 @@ namespace MSBuild.Community.Tasks.DependencyGraph
         /// <summary>
         /// A name for display in a graph
         /// </summary>
-        public string DisplayName { get; private set; }
+        public string DisplayName { get; protected set; }
 
         /// <summary>
         /// Creates new instance
@@ -69,6 +71,8 @@ namespace MSBuild.Community.Tasks.DependencyGraph
     /// </summary>
     public class ProjectReference : BaseReference
     {
+        private string _assemblyName;
+
         /// <summary> 
         /// Path to a project file of reference 
         /// </summary>
@@ -85,6 +89,24 @@ namespace MSBuild.Community.Tasks.DependencyGraph
         public string Name { get; private set; }
 
         /// <summary>
+        /// ProjectGuid property of project file
+        /// </summary>
+        public string ProjectGuid { get; private set; }
+
+        /// <summary>
+        /// AssemblyNAme property of project file
+        /// </summary>
+        public string AssemblyName
+        {
+            get { return _assemblyName; }
+            private set
+            {
+                DisplayName = value;
+                _assemblyName = value;
+            }
+        }
+
+        /// <summary>
         /// Creates new instance
         /// </summary>
         /// <param name="include">The name of the assembly reference</param>
@@ -96,6 +118,27 @@ namespace MSBuild.Community.Tasks.DependencyGraph
             Include = include;
             Project = project;
             Name = name;
+        }
+
+        /// <summary>
+        /// Updates Include folder and convertes it to absolute path
+        /// </summary>
+        /// <param name="baseFolder"></param>
+        public void UpdateInclude(string baseFolder)
+        {
+            if (Path.IsPathRooted(Include))
+                return;
+            Include = Path.GetFullPath(Path.GetDirectoryName(baseFolder) + "\\" + Include);
+        }
+
+        /// <summary>
+        /// updates parser dependent properties: <see cref="P:AssemblyName"/> and <see cref="P:ProjectGuid"/>
+        /// </summary>
+        /// <param name="parser"></param>
+        public void Update(ProjectFileParser parser)
+        {
+            AssemblyName = parser.GetAssemblyName();
+            ProjectGuid = parser.GetGuid();
         }
     }
 }
