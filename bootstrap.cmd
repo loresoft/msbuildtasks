c515363cac8935b22653322aaca3470658a8af5d
Paul Welter <pwelter@loresoft.com>
2016-04-29 10:27:58 -0500

    fix build

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -1,3 +1,5 @@
 @echo off
+Nuget.exe restore "Source\MSBuild.Community.Tasks.sln"
+
 NuGet.exe install MSBuildTasks -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
 NuGet.exe install ilmerge -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
