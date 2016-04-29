50040797b94f91342c1e90e04d51ced51e989bfe
Paul Welter <pwelter@loresoft.com>
2016-04-29 10:25:50 -0500

    more cleanup

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -1,4 +1,3 @@
 @echo off
 NuGet.exe install MSBuildTasks -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
-NuGet.exe install NUnit.Runners -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
 NuGet.exe install ilmerge -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
