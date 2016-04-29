08cca96287998a23e7350e919dae9504fac9a7d8
Paul Welter <pwelter@loresoft.com>
2016-04-29 11:02:50 -0500

    add start of linux support

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -2,4 +2,4 @@
 Nuget.exe restore "Source\MSBuild.Community.Tasks.sln"
 
 NuGet.exe install MSBuildTasks -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
-NuGet.exe install ilmerge -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
+NuGet.exe install ILRepack.MSBuild.Task -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
