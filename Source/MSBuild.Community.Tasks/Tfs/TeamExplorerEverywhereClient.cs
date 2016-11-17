3d00dc59c0831aa269746aab43cfd579c432a549
Bertrand THOMAS <bertrand@devpro.fr>
2016-11-17 17:27:50 +0100

    Added TeamExplorerEverywhereClient Needed to use another toolName (tf.cmd) This class inherits from TfsClient

---
+++ Source/MSBuild.Community.Tasks/Tfs/TeamExplorerEverywhereClient.cs
@@ -0,0 +1,19 @@
+﻿namespace MSBuild.Community.Tasks.Tfs
+{
+    /// <summary>
+    /// A task for Team Foundation Server version control.
+    /// </summary>
+    public class TeamExplorerEverywhereClient : TfsClient
+    {
+        /// <summary>
+        /// Gets the name of the executable file to run.
+        /// </summary>
+        /// <returns>
+        /// The name of the executable file to run.
+        /// </returns>
+        protected override string ToolName
+        {
+            get { return "tf.cmd"; }
+        }
+    }
+}
