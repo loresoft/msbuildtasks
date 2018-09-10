ba5ca59f46a9745e25be3ef749feed0d91f81a8d
Martin Baranski <5111490+Eagle3386@users.noreply.github.com>
2018-09-10 18:15:37 +0200

    Corrected embarrassingly wrong OR-comparism..

--- Source/MSBuild.Community.Tasks/Tfs/TfsClient.cs
+++ Source/MSBuild.Community.Tasks/Tfs/TfsClient.cs
@@ -234,8 +234,11 @@ namespace MSBuild.Community.Tasks.Tfs
 
         private string FindToolPath(string toolName)
         {
-            return candidatePaths.FirstOrDefault(Directory.Exists)
-                || Directory.GetDirectories(@"C:\Program Files (x86)\Microsoft Visual Studio", "Team Explorer", SearchOption.AllDirectories).FirstOrDefault();
+            string candidate = candidatePaths.FirstOrDefault(Directory.Exists)
+            
+            return string.IsNullOrEmpty(candidate)
+                ? Directory.GetDirectories(@"C:\Program Files (x86)\Microsoft Visual Studio", "Team Explorer", SearchOption.AllDirectories).FirstOrDefault()
+                : candidate;
         }
 
         /// <inheritdoc />
