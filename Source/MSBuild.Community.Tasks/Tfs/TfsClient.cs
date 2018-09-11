cd5ac9818c91cb8730d8b4eaef8a22eadec8e177
Martin Baranski <5111490+Eagle3386@users.noreply.github.com>
2018-09-11 12:54:32 +0200

    Prefer VS2017+ candidate over older ones
    Older VS candidate paths might exist, but don't include Team Explorer

--- Source/MSBuild.Community.Tasks/Tfs/TfsClient.cs
+++ Source/MSBuild.Community.Tasks/Tfs/TfsClient.cs
@@ -234,11 +234,9 @@ namespace MSBuild.Community.Tasks.Tfs
 
         private string FindToolPath(string toolName)
         {
-            string candidate = candidatePaths.FirstOrDefault(Directory.Exists);
+            string candidate = Directory.GetDirectories(@"C:\Program Files (x86)\Microsoft Visual Studio", "Team Explorer", SearchOption.AllDirectories).FirstOrDefault();
             
-            return string.IsNullOrEmpty(candidate)
-                ? Directory.GetDirectories(@"C:\Program Files (x86)\Microsoft Visual Studio", "Team Explorer", SearchOption.AllDirectories).FirstOrDefault()
-                : candidate;
+            return string.IsNullOrEmpty(candidate) ? candidatePaths.FirstOrDefault(Directory.Exists) : candidate;
         }
 
         /// <inheritdoc />
